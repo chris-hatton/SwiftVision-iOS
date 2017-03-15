ff8a98ab25184e60427e6a8221524b691a1e5437
Chris Hatton <christopherhattonuk@gmail.com>
2017-03-15 21:42:53 +1100

    Sync

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftVision-iOS”
+    name: "SwiftVision-iOS"
     targets: [],
     dependencies: [
-		.Package(url: “https://github.com/chris-hatton/SwiftVision.git“,	majorVersion: 1),
-        .Package(url: “https://github.com/chris-hatton/SwiftImage.git“,		majorVersion: 1),
-        .Package(url: “https://github.com/chris-hatton/SwiftImage-iOS.git“,	majorVersion: 1)
+		.Package(url: "https://github.com/chris-hatton/SwiftVision.git",	majorVersion: 1),
+        .Package(url: "https://github.com/chris-hatton/SwiftImage.git",		majorVersion: 1),
+        .Package(url: "https://github.com/chris-hatton/SwiftImage-iOS.git",	majorVersion: 1)
     ]
 )
