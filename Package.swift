2929f200190c44bf8f1621b00378aa22bb491308
Chris Hatton <christopherhattonuk@gmail.com>
2017-03-16 21:45:35 +1100

    Submodules configured

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ let package = Package(
     name: "SwiftVision-iOS",
     targets: [],
     dependencies: [
-		.Package(url: "https://github.com/chris-hatton/SwiftVision.git",	majorVersion: 1),
-        .Package(url: "https://github.com/chris-hatton/SwiftImage.git",		majorVersion: 1),
-        .Package(url: "https://github.com/chris-hatton/SwiftImage-iOS.git",	majorVersion: 1)
+	.Package(url: "../SwiftVision",	majorVersion: 1),
+        .Package(url: "../SwiftImage",	majorVersion: 1),
+        .Package(url: "../SwiftImage",	majorVersion: 1)
     ]
 )
